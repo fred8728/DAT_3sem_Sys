397b1a48bc1ec4b14cb19089571244b67f63acba
A O <ahmedothman8@hotmail.com>
2019-11-20 13:59:47 +0100

    to pull

---
+++ frontend/src/SearchPage.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, NavLink, Route } from "react-router-dom";
+//import InputField from "@kiwicom/orbit-components/lib/InputField";
+import apiFacade from "./apiFacade";
+import Login from "./Login";
+import LoggedIn from "./LoggedIn";
+import Recipes from "./Recipes";
+import AllRecipes from "./Recipes";
+
+export default function SearchPage() {
+  // implement a filter method which takes an array and can filter the
+  // the values it needs and filter out them i dont need.
+  return (
+    <div>
+      <form>
+        <input type="text" /> <label> insert a search value</label>
+        <p> test </p>
+        <p> lva</p>
+      </form>
+    </div>
+  );
+}
