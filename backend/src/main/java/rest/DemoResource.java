e7036984999179ce1d2208bd91ae4ba4257b650e
Emil-Skovbo <emilskovbo18@gmail.com>
2019-11-21 12:19:20 +0100

    trying to do lifting state ... help me

--- backend/src/main/java/rest/DemoResource.java
+++ backend/src/main/java/rest/DemoResource.java
@@ -44,7 +44,7 @@ public class DemoResource {
         return "{\"msg\":\"Hello anonymous\"}";
     }
 
-    //Just to verify if the database is setup
+    //Just to  verify if the database is setup
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("all")
--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -1,21 +1,59 @@
 import React, { useState, useEffect } from "react";
 import apiFacade from "./apiFacade";
 
-const AllRecipes = (props) =>{
+const people = [
+  "Siri",
+  "Alexa",
+  "Google",
+  "Facebook",
+  "Twitter",
+  "Linkedin",
+  "Sinkedin"
+];
+
+
+const AllRecipes = (props) => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = e => {
+    setSearchTerm(search : e.target.value);
+  };
+  useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    
-      apiFacade.getRecipes().then(data => {setRecipes(data.results)
-      console.log("check data",data)});
-      
-      
+
+    apiFacade.getRecipes().then(data => {
+      setRecipes(data.results)
+      console.log("check data", data)
+    });
+
+
 
   }, []);
-  
- 
+
+  const search = this.state.searchTerm;
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <ul>
+        {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
       <table>
         <thead>
           <tr>
