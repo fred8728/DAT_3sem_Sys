504ddacf9c0ee95c6bddbd808f660b8ec2095b70
fred8728 <fskn@hotmail.com>
2019-11-21 10:08:22 +0100

    dk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,7 @@ const {recipes, loggedIn} = props;
         <Home />
       </Route>
       <Route path="/recipes">
-        <Recipes />
+        <Recipes recipes={recipes}/>
       </Route>
       <Route path="/signIn">
         <Login loggedIn={loggedIn} />
