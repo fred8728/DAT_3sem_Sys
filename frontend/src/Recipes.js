0a6ae461968db517fe463e1015fcc23c73d6c30d
Emil-Skovbo <emilskovbo18@gmail.com>
2019-11-22 12:00:12 +0100

    made search function with simone

--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -2,73 +2,38 @@ import React, { useState, useEffect } from "react";
 import apiFacade from "./apiFacade";
 import { BrowserRouter, useRouteMatch, useParams, Route, Link } from "react-router-dom";
 
-<<<<<<< HEAD
-const people = [
-  "Siri",
-  "Alexa",
-  "Google",
-  "Facebook",
-  "Twitter",
-  "Linkedin",
-  "Sinkedin"
-];
 
+const AllRecipes = () => {
 
-const AllRecipes = (props) => {
-
+  const [recipes, setRecipes] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  let match = useRouteMatch();
   const handleChange = e => {
-    setSearchTerm(search : e.target.value);
+    setSearchTerm(e.target.value);
+    console.log(recipes)
   };
+
+
   useEffect(() => {
-    const results = people.filter(person =>
-      person.toLowerCase().includes(searchTerm)
-    );
-    setSearchResults(results);
+    const n =
+      apiFacade.getRecipes().then(data => {
+        setRecipes(data.results)
+        console.log("check data", data.results)
+      });
   }, [searchTerm]);
 
 
-=======
-const AllRecipes = () =>{
->>>>>>> 240229eae1dde2098c240c0a34b10a301ceb7aa0
-  const [recipes, setRecipes] = useState([]);
-  let match = useRouteMatch();
-
-  useEffect(() => {
-<<<<<<< HEAD
-
-    apiFacade.getRecipes().then(data => {
-      setRecipes(data.results)
-      console.log("check data", data)
-    });
 
 
-
-=======
-    
-      apiFacade.getRecipes().then(data => {setRecipes(data.results)
-      console.log("check data",data)});
->>>>>>> 240229eae1dde2098c240c0a34b10a301ceb7aa0
-  }, []);
-
-  const search = this.state.searchTerm;
   return (
+
     <div>
-<<<<<<< HEAD
       <input
         type="text"
         placeholder="Search"
         value={searchTerm}
         onChange={handleChange}
       />
-      <ul>
-        {searchResults.map(item => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      <table>
-=======
       <link
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -77,7 +42,6 @@ const AllRecipes = () =>{
       ></link>
       <br></br>
       <table class="table">
->>>>>>> 240229eae1dde2098c240c0a34b10a301ceb7aa0
         <thead>
           <tr>
             <th></th>
@@ -87,18 +51,28 @@ const AllRecipes = () =>{
           </tr>
         </thead>
         <tbody>
-          {recipes.map((data, index) =>(
-            <tr key={index}>
-              <td>{<img src={data.thumbnail} />}</td>
-              <td>{data.title}</td>
-              <td>
-                <Link to={`${match.url}/${data.title}/${data.ingredients}/${data.href}`}>
-                  Details
+          {recipes
+            .filter(recipe => {
+              return (
+                recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())
+
+              )
+            }
+
+            )
+            .map((data, index) => (
+              <tr key={index}>
+                <td>{<img src={data.thumbnail} />}</td>
+                <td>{data.title}</td>
+                <td>
+                  <Link to={`${match.url}/${data.title}/${data.ingredients}/${data.href}`}>
+                    Details
                 </Link>
-              </td>
-              <td>{<a href={data.href}> Link </a>}</td>
-            </tr>
-          ))}
+                </td>
+                <td>{<a href={data.href}> Link </a>}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
       <br></br>
@@ -109,10 +83,10 @@ const AllRecipes = () =>{
   );
 }
 
-function Recipe(){
-  let {picture, title, ingredients, link} = useParams();
-  return(
-    
+function Recipe() {
+  let { picture, title, ingredients, link } = useParams();
+  return (
+
     <div>
       <p>Recipe: {title}</p>
       <p>Ingredients: {ingredients}</p>
