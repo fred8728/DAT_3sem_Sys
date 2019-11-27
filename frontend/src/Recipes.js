463f40c7c3586d0946c2fc5e970ef1e30e067c7f
Emil-Skovbo <emilskovbo18@gmail.com>
2019-11-27 12:12:46 +0100

    fixed some errors

--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -50,10 +50,10 @@ const AllRecipes = () => {
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></link>
       <br></br>
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th></th>
