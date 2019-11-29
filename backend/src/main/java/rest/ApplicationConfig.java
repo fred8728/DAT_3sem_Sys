6e7801ed4847af2f38823b638caece9979466567
fred8728 <fskn@hotmail.com>
2019-11-29 10:48:19 +0100

    dk

--- backend/src/main/java/rest/ApplicationConfig.java
+++ backend/src/main/java/rest/ApplicationConfig.java
@@ -26,6 +26,7 @@ public class ApplicationConfig extends Application {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.DemoResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
+        resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
     }
     
